4962c48dd3ea73b9bbe8f5c51ac9188e40a5b756
Worlaz <wmalteskog@telia.com>
2022-07-31 22:43:39 +0200

    Initial

---
+++ Source/Ashborn/Ashborn.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Ashborn.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ashborn, "Ashborn" );
---
+++ Source/Ashborn/AshbornGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AshbornGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASHBORN_API AAshbornGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
